460abe55fa89c1720a60f20a68bb7621e2276716
kayla8693 <56415756+kayla8693@users.noreply.github.com>
2020-05-12 05:13:24 -0500

    added contact page

--- kayla-gordon-portfolio/src/components/ContentBox.js
+++ kayla-gordon-portfolio/src/components/ContentBox.js
@@ -1,10 +1,12 @@
 import React from "react";
-import About from "./pages/About";
+// import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function ContentBox() {
     return (
         <div className="col-sm-8" id="contentBox">
-            <About />
+            {/* <About /> */}
+            <Contact />
         </div>
     )
 
---
+++ kayla-gordon-portfolio/src/components/pages/Contact.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+function Contact() {
+    return (
+        <>
+            <h2>Contact</h2>
+            <hr />
+            <form className="form">
+                <label>Name</label><br />
+                <input
+                    type="text"
+                    placeholder="John Smith"
+                    className="form-control"
+                    id="name"
+                /><br />
+                <label>Email</label><br />
+                <input
+                    type="text"
+                    placeholder="example@gmail.com"
+                    className="form-control"
+                    id="email"
+                /><br />
+                <label for="comment">Message</label><br />
+                <textarea
+                    rows="5"
+                    className="form-control"
+                    id="comment"
+                /><br />
+                <input 
+                type="button"
+                value="Submit"
+                className="rounded"
+                id="submit"
+                />
+    
+
+            </form>
+
+        </>
+    );
+};
+
+export default Contact;
