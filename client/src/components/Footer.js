000df5451caadaff04af17611592c791a1881d82
kayla8693 <56415756+kayla8693@users.noreply.github.com>
2020-05-23 00:00:24 -0500

    updated portfolio w new app

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,6 +4,12 @@ function Footer() {
     return (
 
         <nav className="navbar fixed-bottom navbar-dark bg-dark footer">
+            <row>
+            <span className="myPhone">phone: 214-469-4905</span>
+            <span className="myEmail">email: kaylag044@gmail.com</span>
+            </row>
+            <br></br>
+
             <span className="footer-text">© 2020 Kayla Gordon. All rights reserved.</span>
         </nav>
     )
