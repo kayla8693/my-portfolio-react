f8c58d94bc3b64050ae6a0a0e08e71f6ef536ab1
kayla8693 <56415756+kayla8693@users.noreply.github.com>
2020-05-23 00:20:47 -0500

    updated portfolio w new app; added contact info to footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,11 +4,9 @@ function Footer() {
     return (
 
         <nav className="navbar fixed-bottom navbar-dark bg-dark footer">
-            <row>
-            <span className="myPhone">phone: 214-469-4905</span>
-            <span className="myEmail">email: kaylag044@gmail.com</span>
-            </row>
-            <br></br>
+            <span className="footer-text">phone: 214-469-4905 | email: kaylag044@gmail.com</span>
+
+
 
             <span className="footer-text">© 2020 Kayla Gordon. All rights reserved.</span>
         </nav>
