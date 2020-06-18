d7bf22a5b513c31de20acbc330eff3034f8dedb3
kayla8693 <56415756+kayla8693@users.noreply.github.com>
2020-06-18 00:42:16 -0500

    updated resume

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return (
 
         <nav className="navbar fixed-bottom navbar-dark bg-dark footer">
-            <span className="footer-text">phone: 214-469-4905 | email: kaylag044@gmail.com</span>
+            <span className="footer-text">phone: 214-469-4905 | email: <a className="footerEmail" href="mailto:kaylag044@gmail.com">kaylag044@gmail.com</a></span>
 
 
 
